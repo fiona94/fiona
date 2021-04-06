f2976164ddacbadd25d4410717e0b5d05b0f545b
fiona <phionafifimaila@gmail.com>
2021-04-06 16:14:46 +0200

    first commit

---
+++ test-tool/src/test/java/maila/test/tool/TestToolApplicationTests.java
@@ -0,0 +1,13 @@
+package maila.test.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
