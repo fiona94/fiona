3fddd2b01fb11b65ea59b4d8318d3c8f62e17fc4
fiona <phionafifimaila@gmail.com>
2021-04-06 16:06:19 +0200

    first commit

---
+++ testing/src/test/java/com/example/servy/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servy.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
